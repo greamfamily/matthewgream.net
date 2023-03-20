ffd21f0ab4ddea1578156f54a1467102e8cbbe12
Gream Family <81447136+greamfamily@users.noreply.github.com>
2023-03-20 08:49:27 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,57 @@
-<?php header( 'Location: /index.htm' ) ;  ?>
+<html lang="en">
+  <head>
+    <title>Matthew Gream [matthewgream.net]</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, width=device-width, height=device-height">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <meta property="og:url" content="http://matthewgream.net">
+    <meta property="og:title" content="Matthew Gream">
+    <meta property="og:image" content="http://matthewgream.net/favicon-194x194.png">
+    <meta property="og:site_name" content="Matthew Gream">
+    <meta property="og:description" content="Matthew Gream, London W2, United Kingdom">
+
+    <meta itemprop="name" content="Matthew Gream">
+    <meta itemprop="description" content="Matthew Gream, London W2, United Kingdom">
+    <meta itemprop="image" content="http://matthewgream.net/favicon-194x194.png">
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="Matthew Gream">
+    <meta name="twitter:description" content="Matthew Gream, London W2, United Kingdom">
+    <meta name="twitter:image" content="http://matthewgream.net/favicon-194x194.png">
+
+    <meta name="description" content="Matthew Gream, London W2, United Kingdom">
+    <meta name="keywords" content="Matthew Gream, Matthew, Gream, MGREAM">
+    <link rel="canonical" href="http://matthewgream.net">
+
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+
+    <meta name="theme-color" content="#ffffff">
+    <meta name="google-site-verification" content="vYd3DabcChAqQ2VV273osiQdJxsA1-cRt2_DaH9DvWk" />
+
+  </head>
+  <body>
+    <table>
+    <tr><td style="font-family:'Roboto',sans-serif;font-size:1000%;font-weight:bold;color:red;padding-left:0.5em;">
+    !
+    </td><td style="font-family:'Roboto',sans-serif;font-size:125%;vertical-align:top;padding-left:1em;">
+		<br><br>
+		Matthew's LinkedIn profile, with up-to-date contact information, is maintained at <a href="https://linkedin.com/in/mgream">https://linkedin.com/in/mgream</a>.
+		<br>
+		This website (a portfolio of earlier work) has not been maintained for over a decade, is still <a href="index-old.htm">available</a>.
+	</td></tr>
+	</table>
+
+	<script type="text/javascript">
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+	var pageTracker = _gat._getTracker("UA-2894404-1");
+	pageTracker._initData();
+	pageTracker._trackPageview();
+	</script>
+  </body>
+</html>
